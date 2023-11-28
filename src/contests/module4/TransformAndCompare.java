07715c2ee8265575e3d89fe08afcaeb761ff862a
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-11-28 20:02:24 +0530

    Transform And Compare added

---
+++ src/contests/module4/TransformAndCompare.java
@@ -0,0 +1,55 @@
+package contests.module4;
+
+import java.util.Scanner;
+
+public class TransformAndCompare {
+
+    //??
+    public static boolean canTransform(String start, String end) {
+        StringBuilder s = new StringBuilder();
+        StringBuilder e = new StringBuilder();
+        for (char c : start.toCharArray()) {
+            if (c != 'X') {
+                s.append(c);
+            }
+        }
+        for (char c : end.toCharArray()) {
+            if (c != 'X') {
+                e.append(c);
+            }
+        }
+        if (!s.toString().contentEquals(e))
+            return false;
+        int count = 0;
+        for (int i = 0; i < start.length(); i++) {
+            if (start.charAt(i) == 'R') {
+                count++;
+            } else if (end.charAt(i) == 'R') {
+                count--;
+            }
+            if (count < 0) {
+                return false;
+            }
+        }
+        count = 0;
+        for (int i = 0; i < start.length(); i++) {
+            if (end.charAt(i) == 'L') {
+                count++;
+            } else if (start.charAt(i) == 'L') {
+                count--;
+            }
+            if (count < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        String e = scanner.nextLine();
+
+        System.out.println(canTransform(s, e));
+    }
+}

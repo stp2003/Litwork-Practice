6ace2a90bb8e58f65bf8db2c520b766b699ecc14
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-11-28 18:23:01 +0530

    Simple Text Editor added

--- src/contests/module3/SimpleTextEditor.java
+++ src/contests/module3/SimpleTextEditor.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class SimpleTextEditor {
 
     public static void main(String[] args) {
+
         CustomStack customStack = new CustomStack();
 
         Scanner scanner = new Scanner(System.in);
@@ -16,65 +17,81 @@ public class SimpleTextEditor {
             String[] parts = command.split(" ");
             String op = parts[0];
 
-            switch (op) {
-                case "1" -> customStack.insert(parts[1]);
-                case "2" -> customStack.delete(Integer.parseInt(parts[1]));
-                case "3" -> customStack.get(Integer.parseInt(parts[1]));
-                case "4" -> customStack.undo();
+            if (op.equals("1")) {
+                customStack.insert(parts[1]);
+            } else if (op.equals("2")) {
+                customStack.delete(Integer.parseInt(parts[1]));
+            } else if (op.equals("3")) {
+                customStack.get(Integer.parseInt(parts[1]));
+            } else if (op.equals("4")) {
+                customStack.undo();
+            }
+        }
+    }
+}
+
+class CustomStack {
+
+    private StringBuilder text;
+    private List<Command> commands;
+
+
+    public CustomStack() {
+
+        this.text = new StringBuilder();
+        this.commands = new ArrayList<>();
+    }
+
+
+    public void insert(String value) {
+
+        text.append(value);
+        commands.add(new Command("delete", value.length()));
+    }
+
+
+    public void delete(int value) {
+
+        String deleted = text.substring(text.length() - value);
+        text.delete(text.length() - value, text.length());
+        commands.add(new Command("insert", deleted));
+    }
+
+
+    public void get(int value) {
+
+        char character = text.charAt(value - 1);
+        System.out.println(character);
+    }
+
+
+    public void undo() {
+
+        if (!commands.isEmpty()) {
+
+            Command lastCommand = commands.remove(commands.size() - 1);
+            if (lastCommand.operation.equals("delete")) {
+                text.append(lastCommand.value);
+            } else if (lastCommand.operation.equals("insert")) {
+                text.delete(text.length() - lastCommand.value.length(), text.length());
             }
         }
     }
 
 
-    static class CustomStack {
-        private final StringBuilder text;
-        private final List<Command> commands;
+    private static class Command {
 
-        public CustomStack() {
-            this.text = new StringBuilder();
-            this.commands = new ArrayList<>();
+        private String operation;
+        private String value;
+
+        public Command(String operation, int value) {
+            this.operation = operation;
+            this.value = String.valueOf(value);
         }
 
-        public void insert(String value) {
-            text.append(value);
-            commands.add(new Command("delete", value.length()));
-        }
-
-        public void delete(int value) {
-            String deleted = text.substring(text.length() - value);
-            text.delete(text.length() - value, text.length());
-            commands.add(new Command("insert", deleted));
-        }
-
-        public void get(int value) {
-            char character = text.charAt(value - 1);
-            System.out.println(character);
-        }
-
-        public void undo() {
-            if (!commands.isEmpty()) {
-                Command lastCommand = commands.remove(commands.size() - 1);
-                if (lastCommand.operation.equals("delete")) {
-                    text.append(lastCommand.value);
-                } else if (lastCommand.operation.equals("insert")) {
-                    text.delete(text.length() - lastCommand.value.length(), text.length());
-                }
-            }
-        }
-
-        private static class Command {
-            private final String operation;
-            private final String value;
-
-            public Command(String operation, int value) {
-                this.operation = operation;
-                this.value = String.valueOf(value);
-            }
-
-            public Command(String operation, String value) {
-                this.operation = operation;
-                this.value = value;
-            }
+        public Command(String operation, String value) {
+            this.operation = operation;
+            this.value = value;
         }
     }
 }
